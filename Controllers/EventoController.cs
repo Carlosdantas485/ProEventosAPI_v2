15122726f1f8133788a667a2c8b85891758fdd63
Carlos Alberto Dantas Filho <carlosdantas485@gmail.com>
2022-12-16 15:45:30 -0300

    first commit

---
+++ Controllers/EventoController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public EventoController()
+        {
+           
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Value";
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Value";
+        }
+
+        [HttpDelete]
+        public string Delete(int id)
+        {
+            return $"Value = {id}";
+        }
+
+        [HttpPut]
+        public string Put(int id)
+        {
+            return $"Value = {id}";
+        }
+    }
+}
